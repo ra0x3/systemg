12ffffb1a8ca6589eeb2291bc77edeeacfafc013
ra0x3 <spam.rashad@protonmail.com>
2026-08-22 17:06:03 -0700

    feat(web): load the Google Ads gtag on every sysg.dev page

---
+++ sysg.dev/app/ds/analytics.ts
@@ -0,0 +1,3 @@
+export const GTAG_ID = "AW-18405236259";
+
+export const gtagInitScript = `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date());gtag("config","${GTAG_ID}")`;
